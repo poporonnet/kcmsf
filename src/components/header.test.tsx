d80820c8bc6377aacf05b68e9a72f286817acc1b
kiharu3112 <kijiharu3112@outlook.jp>
2024-04-02 17:27:16 +0900

    feat:サンプルとしてheaderのちょっとしたテスト

---
+++ src/components/header.test.tsx
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "vitest";
+import { render } from "../../test";
+import { Header } from "./header.tsx";
+import { screen } from "@testing-library/react";
+import { AppShell } from "@mantine/core";
+import '@testing-library/jest-dom';
+
+describe('header component', () => {
+  test('has /entrylist link', () => {
+    render(
+      <AppShell
+        header={{ height: 60, offset: true }}
+        footer={{ height: 30, offset: true }}
+        padding="md"
+      >
+        <Header />
+        </AppShell>
+    );
+
+    expect(screen.getByText('参加者一覧')).toHaveAttribute(
+      'href',
+      '/entrylist'
+    )
+    expect(screen.getByText('参加者登録')).toHaveAttribute(
+      'href',
+      '/entry'
+    )
+    expect(screen.getByText('試合表')).toHaveAttribute(
+      'href',
+      '/matchlist'
+    )
+    expect(screen.getByText('試合結果')).toHaveAttribute(
+      'href',
+      '/result'
+    )
+    expect(screen.getByText('ランキング')).toHaveAttribute(
+      'href',
+      '/ranking'
+    )
+  })
+})
